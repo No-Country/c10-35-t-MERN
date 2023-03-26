c67ba7c1c57192d24b92e7c9d0f297736dbe6930
javier marquez <hello.jmarquez@gmail.com>
2023-03-26 15:36:15 -0500

    feat: React 18 installed, eslint+prettier configured

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>www.stoker.com</h1>
+			<h2>Sitio en construcción</h2>
+			<p>Próximamente...</p>
+		</div>
+	)
+}
+
+export default App
