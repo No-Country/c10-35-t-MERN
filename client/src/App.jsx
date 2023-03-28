0f4ca1e5083028db8e8119f40e31427d6f0a80cd
javier marquez <hello.jmarquez@gmail.com>
2023-03-27 20:19:12 -0500

    feature: tailwind installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 	return (
 		<div className='App'>
-			<h1>www.stoker.com</h1>
+			<h1 className='text-yellow-300'>www.stoker.com</h1>
 			<h2>Sitio en construcción</h2>
 			<p>Próximamente...</p>
 		</div>
