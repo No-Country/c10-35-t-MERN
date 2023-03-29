d3ae5b88bf32ce24fc233161a3b0dab53fc2ec75
javier marquez <hello.jmarquez@gmail.com>
2023-03-29 14:11:22 -0500

    style: added login layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Login from './components/Login'
 function App() {
 	return (
 		<div className='App text-center'>
-			<h1 className='text-slate-400'>www.stoker.com</h1>
 			<Login />
 		</div>
 	)
