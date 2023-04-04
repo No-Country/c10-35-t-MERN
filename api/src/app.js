962056a3d01861fbcda930386451ece854615120
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2023-04-03 23:05:02 -0300

    route register con bug

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,58 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import { PORT } from "./config/config.js";
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+const PORT = process.env.PORT || 3000
+
+const Users = require('./models/users');
 
 const app = express();
 
 app.set("port", PORT);
 
-app.use(morgan("dev"));
+app.use(morgan("dev")); 
 app.use(cors());
+app.use(express.json());
 
-export default app;
+// Rutas
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body; 
+  // Comprobar si los campos usuario y contraseña existen
+  if (!username || !password) {
+    return res.status(400).send('Debes proporcionar un nombre de usuario y contraseña');
+  }
+  console.log('Model user',Users)
+  try {
+    // Comprobar si el usuario ya existe en la base de datos
+    // const existingUser = await Users.findOne({ username });
+    // if (existingUser) {
+    //   return res.status(409).send('Este usuario ya existe');
+    // } 
+
+    // Hashear la contraseña
+    const salt = await bcrypt.genSalt(10);
+    // console.log(salt)
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Crear un nuevo usuario con la contraseña hasheada
+    // const newUser = new Users({
+    //   username,
+    //   password: hashedPassword
+    // });
+    // await newUser.save();
+
+    // Devolver un mensaje de éxito
+    res.status(201).send('Usuario creado con éxito');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Ha ocurrido un error al crear el usuario');
+  }
+});
+
+app.get('/',(req,res)=>{
+  res.send('Bienvenidos a Stocker')
+})
+
+
+
+module.exports = app
--- api/src/models/users.js
+++ api/src/models/users.js
@@ -1,6 +1,7 @@
-'use strict';
-import { Model } from 'sequelize';
-export default (sequelize, DataTypes) => {
+"use strict";
+// import { Model } from 'sequelize';
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
     /**
      * Helper method for defining associations.
@@ -10,43 +11,46 @@ export default (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Users.hasMany(models.Tokens, {
-        foreignKey: 'userId',
-        as: 'tokens'
-      })
+        foreignKey: "userId",
+        as: "tokens",
+      });
       Users.hasMany(models.Category_Users, {
-        foreignKey: 'userId',
-        as: 'category_users'
-      })
+        foreignKey: "userId",
+        as: "category_users",
+      });
     }
   }
-  Users.init({
-    full_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  Users.init(
+    {
+      full_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isAvailable: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password_hash: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    isAvailable: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
+    {
+      sequelize,
+      modelName: "Users",
     }
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
+  );
   return Users;
 };
