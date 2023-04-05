f5baaa5724aafdc64a979f403a748cfa4b88f7f7
Xch4rt <pab203.guti@gmail.com>
2023-04-04 19:18:20 -0600

    add: config file in server class

---
+++ api/src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+// Routes files
+const brandRoute = require("./routes/brands");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json(
+    {
+        limit: "20mb"
+    }
+));
+
+// Mount routers
+app.use("/api/v1/brands", brandRoute);
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port: ${server.address().port}`);
+});
+
