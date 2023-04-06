ef1a6c8cf3cb0a109a34ce9b185eff7cffea007f
yesi fita <yesifita@gmail.com>
2023-04-06 19:56:20 -0300

    fix: change imputs

--- client/src/components/textBox/TextBoxLg.jsx
+++ client/src/components/textBox/TextBoxLg.jsx
@@ -1,12 +1,19 @@
-const TextBobxLg = (handleChange) => {
+const TextBobxLg = (props) => {
+	const { label, type, name, state, setState } = props
+
+	const onChange = e => {
+		setState(...state, e.target.value)
+	}
 	return (
-		<label className='m-2 md:m-4 '>
+		<label htmlFor={name}   className='m-2 md:m-4 '>{label}
 			<input
-				name='name'
-				type='text'
-				value=""
-				onChange={handleChange}
+				name={name}
+				id={name}
+				type={type}
 				placeholder='Nombre del Producto'
+				value={state}
+				required
+				onChange={onChange}
 				className='w-ww343 h-8 md:w-44  md:h-10 pl-2  border-2 border-solid rounded-lg text-sm  text-center  md:text-lg flex justify-start items-center'
 			></input>
 		</label>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
+import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './components/Layout'
-import Dashboard from './components/Dashboard/Dashboard'
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children:[
-			{
-				path:'/dashboard',
-				element:<Dashboard/>
-			}
-		]
-	},
-])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App/>
 	</React.StrictMode>
 )
