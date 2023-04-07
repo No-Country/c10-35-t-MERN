c1e8d4adf610d8ab99146106720df692dfcda0b6
Xch4rt <pab203.guti@gmail.com>
2023-04-07 04:37:30 -0600

    add: Categories Service

---
+++ api/src/controllers/categories.controller.js
@@ -0,0 +1,50 @@
+const service = require('../services/categories.service');
+const { errorHandler } = require('../utils/errors');
+
+const create = async (req, res) => {
+    try {
+        return res.status(201).json(await service.create(req.body));
+    } catch (error) {
+        errorHandler(error, res);
+    }
+};
+
+const getAll = async (req, res) => {
+    try {
+        return res.status(200).json(await service.findAll());
+    } catch (error) {
+        errorHandler(error, res);
+    }
+};
+
+const getAllByUserId = async (req, res) => {
+    try {
+        return res.status(200).json(await service.findAllByUserId(req.params.userId));
+    } catch (error) {
+        errorHandler(error, res);
+    }
+};
+
+const deleteCategory = async (req, res) => {
+    try {
+        return res.status(200).json(await service.deleteCategory(req.body));
+    } catch (error) {
+        errorHandler(error, res);
+    }
+};
+
+const updateCategory = async (req, res) => {
+    try {
+        return res.status(200).json(await service.updateCategory(req.body));
+    } catch (error) {
+        errorHandler(error, res);
+    }
+};
+
+module.exports = {
+    create,
+    getAll,
+    getAllByUserId,
+    deleteCategory,
+    updateCategory,
+};
--- api/src/routes/app.routes.js
+++ api/src/routes/app.routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('../routes/users.routes')
 const brandRoutes = require('../routes/brands.routes')
+const categories = require('../routes/categories.routes')
 
 router.use('/users',userRoutes)
 router.use('/brands',brandRoutes)
+router.use('/categories',categories)
 
 module.exports = router
