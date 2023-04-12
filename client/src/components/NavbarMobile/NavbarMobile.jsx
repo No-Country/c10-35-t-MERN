3c1ca4d10d65339fc8020c9d537d0f548da87089
ValentinaBS <vbsanchez16@gmail.com>
2023-04-12 16:53:22 -0300

    fix: active state styles

--- client/src/components/NavbarMobile/NavbarMobile.jsx
+++ client/src/components/NavbarMobile/NavbarMobile.jsx
@@ -5,9 +5,9 @@ export default function NavbarMobile() {
     return (
         <nav className='sticky bottom-0 left-0 z-50 flex justify-between w-full px-4 py-2 text-secundario bg-white'>
             <NavLink to='/inicio' className={({ isActive }) =>
-                isActive ? "rounded-md bg-secundario text-acento" : ""
+                isActive ? "rounded-md bg-secundario text-acento [&>button>span]:text-white" : ""
             }>
-                <button className='group flex flex-col items-center justify-center w-16 h-16 p-2 transition duration-200 ease-in-out border rounded-lg flex-column font-secundaria border-secundario focus:bg-secundario active:bg-secundario active:text-acento active:last:text-white'>
+                <button className='group flex flex-col items-center justify-center w-16 h-16 p-2 transition duration-200 ease-in-out border rounded-lg flex-column font-secundaria border-secundario focus:bg-secundario'>
                     <RiDashboardLine className='group-focus:text-acento w-6 h-6'/>
                     <span className='group-focus:text-white mt-1 iconos'>
                         Inicio
@@ -15,9 +15,9 @@ export default function NavbarMobile() {
                 </button>
             </NavLink>
             <NavLink to='/inventario' className={({ isActive }) =>
-                isActive ? "rounded-md bg-secundario text-acento" : ""
+                isActive ? "rounded-md bg-secundario text-acento [&>button>span]:text-white" : ""
             }>
-                <button className='group flex flex-col items-center justify-center w-16 h-16 p-2 transition duration-200 ease-in-out border rounded-lg flex-column font-secundaria border-secundario focus:bg-secundario active:bg-secundario active:text-acento active:last:text-white'>
+                <button className='group flex flex-col items-center justify-center w-16 h-16 p-2 transition duration-200 ease-in-out border rounded-lg flex-column font-secundaria border-secundario focus:bg-secundario'>
                     <RiFileTextLine className='group-focus:text-acento w-6 h-6'/>
                     <span className='group-focus:text-white mt-1 iconos'>
                         Inventario
@@ -25,9 +25,9 @@ export default function NavbarMobile() {
                 </button>
             </NavLink>
             <NavLink to='/notificaciones' className={({ isActive }) =>
-                isActive ? "rounded-md bg-secundario text-acento" : ""
+                isActive ? "rounded-md bg-secundario text-acento [&>button>span]:text-white" : ""
             }>
-                <button className='group flex flex-col items-center justify-center w-16 h-16 p-2 transition duration-200 ease-in-out border rounded-lg flex-column font-secundaria border-secundario focus:bg-secundario active:bg-secundario active:text-acento active:last:text-white'>
+                <button className='group flex flex-col items-center justify-center w-16 h-16 p-2 transition duration-200 ease-in-out border rounded-lg flex-column font-secundaria border-secundario focus:bg-secundario'>
                     <RiNotification3Line className='group-focus:text-acento w-6 h-6'/>
                     <span className='group-focus:text-white mt-1 iconos'>
                         Notificaciones
