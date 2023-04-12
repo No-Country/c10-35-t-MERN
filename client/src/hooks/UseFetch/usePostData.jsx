1da60771fe88d0e38bcf68c6def80612b87210ea
javier marquez <hello.jmarquez@gmail.com>
2023-04-11 19:15:37 -0500

    feat: added get, post & put fetch hooks

---
+++ client/src/hooks/UseFetch/usePostData.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const usePostData = () => {
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
+	const [responseData, setResponseData] = useState(null)
+
+	const handlePost = async (url, obj, e) => {
+		e.preventDefault()
+		setIsLoading(true)
+		setError(null)
+		setResponseData(null)
+
+		await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(obj),
+		})
+			.then(res => res.json())
+			.then(res => setResponseData(res))
+			.catch(err => setError(err))
+			.finally(setIsLoading(false))
+	}
+
+	return {
+		isLoading,
+		error,
+		responseData,
+		handlePost,
+	}
+}
+export default usePostData
