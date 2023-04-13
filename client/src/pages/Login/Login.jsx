9206826c3955ac0e19762408677c36b7241ab552
javier marquez <hello.jmarquez@gmail.com>
2023-04-13 15:50:34 -0500

    feat: onboarding component integrated

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -11,56 +11,61 @@ function Login() {
 		navigate('/inicio')
 	}
 	return (
-		<Onboarding />
-		// <section className='flex flex-col h-screen py-7 text-center '>
-		// 	<p className='font-secundaria font-normal'>Bienvenidos a</p>
-		// 	<h1>STOKER</h1>
-		// 	<h6>Inicia sesión</h6>
-		// 	<form
-		// 		id='form'
-		// 		action=''
-		// 		className=' flex flex-col items-center w-full h-full gap-y-2'
-		// 	>
-		// 		<div className='flex flex-col  w-4/5 justify-between gap-y-1'>
-		// 			<label htmlFor='user' className='text-left text-xs'>
-		// 				Usuario:
-		// 			</label>
-		// 			<input
-		// 				type='text'
-		// 				name='user'
-		// 				id='user'
-		// 				placeholder='Ingrese usuario'
-		// 			/>
-		// 		</div>
-		// 		<div className='flex flex-col w-4/5 justify-between gap-y-1'>
-		// 			<label htmlFor='password' className='text-left text-xs'>
-		// 				Contraseña:
-		// 			</label>
-		// 			<input
-		// 				type='password'
-		// 				name='password'
-		// 				id='password'
-		// 				placeholder='Ingrese contraseña'
-		// 			/>
-		// 		</div>
-		// 		<div className='flex h-32 justify-center text-center mt-auto w-4/5 relative border-t-2 gap-4'>
-		// 			<p className='text-f12 bg-white absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-2'>
-		// 				O ingresa con
-		// 			</p>
-		// 			<GrFacebook className='mt-6 text-label text-3xl' />
-		// 			<GrGoogle className='mt-6 text-label text-3xl' />
-		// 		</div>
-		// 		<input
-		// 			type='submit'
-		// 			name=''
-		// 			id='submit'
-		// 			value='Iniciar sesión'
-		// 			onClick={handleSubmit}
-		// 			className='bg-gray-300 py-3 rounded-xl w-4/5 font-bold'
-		// 		/>
-		// 		<p className='text-f12'>¿Aún no tienes cuenta?</p>
-		// 	</form>
-		// </section>
+		<>
+			<Onboarding />
+			<section
+				id='login'
+				className='hidden flex flex-col h-screen py-7 text-center '
+			>
+				<p className='font-secundaria font-normal'>Bienvenidos a</p>
+				<h1>STOKER</h1>
+				<h6>Inicia sesión</h6>
+				<form
+					id='form'
+					action=''
+					className=' flex flex-col items-center w-full h-full gap-y-2'
+				>
+					<div className='flex flex-col  w-4/5 justify-between gap-y-1'>
+						<label htmlFor='user' className='text-left text-xs'>
+							Usuario:
+						</label>
+						<input
+							type='text'
+							name='user'
+							id='user'
+							placeholder='Ingrese usuario'
+						/>
+					</div>
+					<div className='flex flex-col w-4/5 justify-between gap-y-1'>
+						<label htmlFor='password' className='text-left text-xs'>
+							Contraseña:
+						</label>
+						<input
+							type='password'
+							name='password'
+							id='password'
+							placeholder='Ingrese contraseña'
+						/>
+					</div>
+					<div className='flex h-32 justify-center text-center mt-auto w-4/5 relative border-t-2 gap-4'>
+						<p className='text-f12 bg-white absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-2'>
+							O ingresa con
+						</p>
+						<GrFacebook className='mt-6 text-label text-3xl' />
+						<GrGoogle className='mt-6 text-label text-3xl' />
+					</div>
+					<input
+						type='submit'
+						name=''
+						id='submit'
+						value='Iniciar sesión'
+						onClick={handleSubmit}
+						className='bg-gray-300 py-3 rounded-xl w-4/5 font-bold'
+					/>
+					<p className='text-f12'>¿Aún no tienes cuenta?</p>
+				</form>
+			</section>
+		</>
 	)
 }
 
--- client/src/pages/Onboarding/Onboarding.jsx
+++ client/src/pages/Onboarding/Onboarding.jsx
@@ -1,35 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/logo_Stocker.png'
-import intro1 from '../../assets/ilustracion_intro1.png'
+import intro1 from '../../assets/onboarding1.png'
+import intro2 from '../../assets/onboarding2.png'
+import intro3 from '../../assets/onboarding3.png'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperNavButtons from './SwiperNavButtons'
 
 function Onboarding() {
+	const [index, setIndex] = useState(0)
+	const handleSkip = () => {
+		const onBoarding = document.getElementById('onboarding')
+		const login = document.getElementById('login')
+		onBoarding.classList.add('hidden')
+		login.classList.remove('hidden')
+	}
+
 	return (
-		<section className='w-10/12 ml-auto mr-auto mt-10'>
+		<section id='onboarding' className='w-10/12 ml-auto mr-auto h-screen'>
 			<header className='flex flex-row-reverse mb-12'>
-				<button className='bg-secundario py-2 px-4 rounded-xl text-primario'>
+				<button
+					onClick={handleSkip}
+					className='bg-secundario py-2 px-4 rounded-xl text-primario'
+				>
 					Saltar
 				</button>
 			</header>
-			<main className='flex flex-col items-center gap-7'>
-				<div>
+			<main className='h-full'>
+				<div className='flex justify-center mb-8'>
 					<img src={logo} alt='Stocker logo' />
 				</div>
-				<div>
-					<img src={intro1} alt='slider intro 1' />
-				</div>
-				<div>
-					<h2>Titulo de imagen</h2>
-					<p className='text-f12'>
-						Tendrás organizados y en un mismo sitio tus entradas y salidas.
-					</p>
-				</div>
+				<Swiper
+					className=' h-full pb-10 '
+					modules={[Navigation, Pagination, Scrollbar, A11y]}
+					spaceBetween={20}
+					slidesPerView={1}
+					navigation={false}
+					pagination={{ clickable: true }}
+					onSlideChange={swiper => setIndex(swiper.activeIndex)}
+					// onSwiper={swiper => console.log(swiper)}
+				>
+					<SwiperSlide>
+						<div className='flex flex-col items-center'>
+							<img src={intro1} alt='' />
+							<div>
+								<h2>Registra tus ingresos y egresos fácilmente</h2>
+								<p>
+									Tendrás organizados y en un mismo sitio tus entradas y
+									salidas.
+								</p>
+							</div>
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className='flex flex-col  items-center'>
+							<img src={intro2} alt='' />
+							<div>
+								<h2>Tienes tu inventario siempre al día</h2>
+								<p>
+									Controlar tu inventario y tenerlo al día será cuestión de un
+									click.
+								</p>
+							</div>
+						</div>
+					</SwiperSlide>
+					<SwiperSlide>
+						<div className='flex flex-col  items-center'>
+							<img src={intro3} alt='' />
+							<div>
+								<h2>Tienes los datos de tu almacén a la mano</h2>
+								<p>
+									Toda la información de tu almacén la tendrás al día y podrás
+									tomar decisiones de negocio seguras.
+								</p>
+							</div>
+						</div>
+					</SwiperSlide>
+					<SwiperNavButtons index={index} />
+				</Swiper>
 			</main>
-			<div>
-				<div className='arrow'>flecha</div>
-				<div className='dots'>
-					<p>punto</p>
-				</div>
-			</div>
 		</section>
 	)
 }
