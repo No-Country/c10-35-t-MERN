0a6cb0444afc157f3da52073ad26b5c5ac1a9332
javier marquez <hello.jmarquez@gmail.com>
2023-04-12 22:48:24 -0500

    fix: small fix in reports

--- client/src/components/Routes/ListRoutes.jsx
+++ client/src/components/Routes/ListRoutes.jsx
@@ -9,7 +9,7 @@ import CreateProducts2 from '../../pages/CreateProducts/CreateProducts2'
 import Ingresos from '../../pages/Ingresos/Ingresos'
 import Egresos from '../../pages/Egresos/Egresos'
 import ProductosMover from '../../pages/ProductosMover/ProductosMover'
-
+import { data } from '../../data'
 const ListRoutes = () => {
 	return (
 		<>
@@ -17,18 +17,18 @@ const ListRoutes = () => {
 				<Routes>
 					<Route path='/*' element={<h1>not found</h1>} />
 					<Route path='/' element={<Login />} />
-					<Route path='/inicio' element={<Dashboard/>} />
-					<Route path='/inventario' element={<Inventary/>} />
+					<Route path='/inicio' element={<Dashboard />} />
+					<Route path='/inventario' element={<Inventary />} />
 					<Route path='/crear-producto' element={<CreateProducts2 />} />
 					<Route path='/registro-de-movimientos' element={<DashboardPopup />} />
-					<Route path='/reportes' element={<Reports />} />
+					<Route path='/reportes' element={<Reports charData={data} />} />
 					<Route path='/perfil' element={<h1>perfil</h1>} />
 					<Route path='/configuracion' element={<h1>configuración</h1>} />
 					<Route path='/detalles' element={<h1>detalles</h1>} />
 					<Route path='/logout' element={<h1>Cerrar sesión</h1>} />
-					<Route path='/ingresos' element={<Ingresos/>} />
-					<Route path='/egresos' element={<Egresos/>} />
-					<Route path='/productoMover' element={<ProductosMover/>} />
+					<Route path='/ingresos' element={<Ingresos />} />
+					<Route path='/egresos' element={<Egresos />} />
+					<Route path='/productoMover' element={<ProductosMover />} />
 				</Routes>
 			</div>
 		</>
