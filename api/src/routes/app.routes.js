ce62dfce1b2832eb7eadca0fa2219c7368ead324
Alben Bustamante <albenbustamante@gmail.com>
2023-04-14 14:38:03 -0500

    added the order routes

--- api/src/routes/app.routes.js
+++ api/src/routes/app.routes.js
@@ -4,10 +4,12 @@ const userRoutes = require('../routes/users.routes');
 const brandRoutes = require('../routes/brands.routes');
 const categories = require('../routes/categories.routes');
 const product = require('../routes/products.routes');
+const orderRoutes = require('../routes/orders.routes');
 
 router.use('/users', userRoutes);
 router.use('/brands', brandRoutes);
 router.use('/categories', categories);
 router.use('/products', product);
+router.use('/orders', orderRoutes);
 
 module.exports = router;
