31dbd58652a758c1c86b190c84aaab67c8ae5a4e
javier marquez <hello.jmarquez@gmail.com>
2023-04-16 20:38:49 -0500

    fix: small fix in display

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -71,7 +71,7 @@ function Login() {
 
 			<main
 				id='login'
-				className='flex flex-col justify-start h-screen w-full px-4 pt-90 pb-4 text-center box-border'
+				className='hidden flex flex-col justify-start h-screen w-full px-4 pt-90 pb-4 text-center box-border'
 			>
 				{isLoading && <h1>Cargando...</h1>}
 				<div className='w-238.33 h-fit flex flex-col items-center'>
