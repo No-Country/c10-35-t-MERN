20bbc2a48e9e47e27c900095ca741209ae2f4672
javier marquez <hello.jmarquez@gmail.com>
2023-04-19 14:13:34 -0500

    fix: fix layout desktop

--- client/src/pages/Reports/Reports.jsx
+++ client/src/pages/Reports/Reports.jsx
@@ -14,33 +14,34 @@ function Reports({ charData }) {
 			<div>
 				<NavBar navBarTile='Reportes' />
 				<div className='flex flex-col justify-center items-center w-full py-5 lg:px-10'>
-					<div className='flex flex-col px-30 py-2.5 rounded-lg border-solid border-1 shadow-sombra border-black mb-5 text-center lg:w-566 lg:h-184 lg:reounded-2xl'>
-						<h2 className='text-secundario mb-2'>Stock total</h2>
-						<div className='flex justify-between lg:justify-center  lg:w-238'>
+					<div className='flex flex-col px-30 py-2.5 rounded-lg border-solid border-1 shadow-sombra border-black mb-5 text-center lg:flex-row lg:flex-wrap lg:justify-between lg:w-566 lg:h-184 lg:reounded-12 lg:bg-primario lg:border-0 lg:px-9 lg:py-5'>
+						<h2 className='text-secundario mb-2 lg:w-full'>Stock total</h2>
+						<div className=' flex justify-between lg:justify-center lg:w-238'>
 							<h3 className='text-left text-acento lg:mr-8'>Total:</h3>
-							<span className='font-secundaria text-xl font-bold text-secundario'>
+							<span className='lg:hidden font-secundaria text-xl font-bold text-secundario'>
 								$100
 							</span>
+							<span className='font-secundaria text-xl font-bold text-secundario'>
+								$10000000
+							</span>
 						</div>
-						<div className='flex justify-between lg:justify-center  lg:w-238'>
+						<div className='flex justify-between lg:justify-center  lg:w-132'>
 							<h3 className='text-left text-acento lg:mr-4'>UND:</h3>
 							<span className='font-secundaria text-xl font-bold text-secundario'>
 								$2000
 							</span>
 						</div>
-						<div className='hidden lg:flex bg-primario w-full py-5 justify-center'>
-							<div className='flex justify-between items-center p-2 rounded-sm bg-white rounded-xl shadow-sombra mb-2 w-11/12'>
-								<h5 className='text-left text-secundario'>
-									Productos baja rotación
-								</h5>
-								<span className='font-secundaria text-f16 font-bold text-acento'>
-									UND 200
-								</span>
-							</div>
+						<div className='hidden lg:flex bg-white lg:shadow-sombra lg:m-auto lg:justify-around lg:py-3 lg:w-343 lg:rounded-12'>
+							<h5 className='text-left text-secundario'>
+								Productos baja rotación
+							</h5>
+							<span className='font-secundaria text-f16 font-bold text-acento'>
+								UND 200
+							</span>
 						</div>
 					</div>
 					<div className='flex relative justify-center items-center w-full bg-primario lg:bg-white'>
-						<div className='flex w-4/5 absolute -top-2 justify-center items-center gap-3 h-11 w-full'>
+						<div className='flex w-4/5 absolute -top-2 lg:-top-6 justify-center items-center gap-3 h-11 '>
 							<button
 								href='#'
 								className='text-f16 font-secundaria text-xl font-bold text-primario px-8 rounded-xl h-full bg-secundario shadow-lg'
@@ -78,8 +79,11 @@ function Reports({ charData }) {
 					<main className='flex bg-primario justify-evenly items-start w-full  gap-2 px-4 py-3 lg:justify-between lg:p-0 lg:bg-white mb-10'>
 						<article className='flex flex-col text-center w-1/2 lg:w-447 '>
 							<h3 className='lg:text-left'>Ingresos y egresos</h3>
-							<div className='bg-white shadow-sombra w-full '>
+							<div className='bg-white shadow-sombra w-full lg:hidden '>
 								<BarChart charData={charData} />
+							</div>
+							<div className='bg-white rounded-xl h-16 lg:h-157  p-2 shadow-sombra'>
+								<CakeChart charData={charData} />
 							</div>
 						</article>
 						<article className=' flex flex-col justify-center  text-center w-1/2 lg:w-446'>
