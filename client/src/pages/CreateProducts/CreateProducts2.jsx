2ce3ac1af5197f6e55998144f77d9dc0e9f1a0a9
yesi fita <yesifita@gmail.com>
2023-04-18 21:33:17 -0300

    refactor: add cuarto grupo-botones fianles

--- client/src/pages/CreateProducts/CreateProducts2.jsx
+++ client/src/pages/CreateProducts/CreateProducts2.jsx
@@ -11,7 +11,6 @@ import Headings from '../../components/CreateProducts/Headings'
 import ModalExcel from '../../components/Modals/ModalExcel'
 import ModalFallaCarga from '../../components/Modals/ModalFallaCarga'
 
-
 const initialForm = {
 	id: Date.now(),
 	nombre: '',
@@ -100,23 +99,19 @@ const CreateProducts2 = () => {
 	const [dataToEdit, setDataToEdit] = useState(null)
 	const [modal, setModal] = useState(false)
 
-
 	const crud = helpFetch()
 	const urlGet = 'http://localhost:3000/data'
 	useEffect(() => {
-
 		fetch(urlGet).then(res => {
-
 			if (!res.err) {
 				console.log(res)
 				setDb(res)
 				setResponse(res)
 			} else {
-				
 				setDb(null)
 				setResponse(res)
 			}
-	})
+		})
 	}, [urlGet])
 
 	const handleChange = e => {
@@ -133,7 +128,7 @@ const CreateProducts2 = () => {
 	const handleSubmit = e => {
 		e.preventDefault()
 		setErrors(validationsForm(form))
-		
+
 		if (Object.keys(errors).length === 0) {
 			helpFetch()
 				.post(urlGet, {
@@ -154,7 +149,6 @@ const CreateProducts2 = () => {
 		if (form.id === null) {
 			return createData(form)
 		} else {
-			
 			return updateData(form)
 		}
 		// handleReset()
@@ -169,9 +163,7 @@ const CreateProducts2 = () => {
 			.then(res => {
 				if (!res.err) {
 					setDb([...db, res])
-				}else(
-					setResponse(res)
-				)
+				} else setResponse(res)
 			})
 	}
 
@@ -186,15 +178,15 @@ const CreateProducts2 = () => {
 			.then(res => {
 				if (!res.err) {
 					let newData = db.map(el => (el.id === data.id ? data : el))
-					setDb(newData);
-				}else{
-					setResponse(res);
+					setDb(newData)
+				} else {
+					setResponse(res)
 				}
 			})
 	}
 
 	const deleteData = id => {
-		let isDelete =confirm(`¿Estas seguro que quieres eliminar ${id}?`)
+		let isDelete = confirm(`¿Estas seguro que quieres eliminar ${id}?`)
 
 		if (isDelete) {
 			let endpoint = `${urlGet}/${id}`
@@ -205,15 +197,15 @@ const CreateProducts2 = () => {
 					if (!res.err) {
 						let newData = db.filter(el => el.id !== id)
 						setDb(newData)
-					}else{
-						setResponse(res);
+					} else {
+						setResponse(res)
 					}
 				})
 		}
 	}
 	const handleReset = e => {
-		setForm(initialForm);
-        setDataToEdit(null);
+		setForm(initialForm)
+		setDataToEdit(null)
 	}
 
 	return (
@@ -231,163 +223,198 @@ const CreateProducts2 = () => {
 					<BoxImage />
 
 					{/* --------------contenedor de fomulario--------------- */}
+
 					<div className=''>
 						<form onSubmit={handleSubmit}>
 							{/* -------contenerdor de inputs----- */}
 
 							<div
-			className='
+								className='
 		w-375 h-469  mt-343 left-0 bg-primario3 absolute rounded-tr-3xl rounded-tl-3xl md:w-566 md:h-418 md:gap-4 md:flex-none md:grow-0 md:order-none md:flex md:flex-col md:ml-16  md:bg-white md:mt-280'
-		>
-			{/* --------------input nombre------------- */}
-			<div className=' w-341 h-88 flex flex-col m-1 pl-2 gap-2 items-center md:w-607 md:mt-0 md:mb-0 md:h-18 md:pb-2'>
-				<div>
-					<div className='flex flex-col ml-25 pl-2'>
-						<label className='w-48 h-6 text-start'>Nombre</label>
-						<input
-							type='text'
-							id='nombre'
-							name='nombre'
-							value={form.nombre}
-							onBlur={handleBlur}
-							onChange={handleChange}
-							className='w-375 h-h48 bg-white border-solid border-1 border-secundario3 rounded-xl px-3 py-4 box-border md:w-556'
-						></input>
-					</div>
-					{errors.nombre && (
-						<p id='msgerror' className='ml-34'>
-							{errors.nombre}
-						</p>
-					)}
-				</div>
-			</div>
-			<div className='flex flex-col justify-center items-center w-screen'>
-				{/* -------------------primer grupo--------------- */}
-
-				<div id='groupInput'>
-					<div>
-						<div id='divPrueba' className=''>
-							<label id='labelPrueba'>Cantidad</label>
-							<input
-								id='inputPrueba'
-								type='number'
-								name='cantidad'
-								value={form.cantidad}
-								onBlur={handleBlur}
-								onChange={handleChange}
-							></input>
-						</div>
-						{errors.cantidad && <p id='msgerror'>{errors.cantidad}</p>}
-					</div>
-					<div>
-						<div id='divPrueba'>
-							<label id='labelPrueba'>Seleciona la unidad</label>
-							<select id='inputPrueba' name='unidades' onChange={handleChange}>
-								<option id='' value=''>
-									Seleciona unidad
-								</option>
-								<option id='unidades' value='unidades'>
-									Unidades
-								</option>
-								<option id='Kg' value='Kg'>
-									Kg
-								</option>
-								<option id='Mts' value='Mts'>
-									Mts
-								</option>
-								<option id='Lts' value='Lts'>
-									Lts
-								</option>
-							</select>
-						</div>
-						{errors.unidades && <p id='msgerror'>{errors.unidades}</p>}
-					</div>
-				</div>
-
-				{/* -------------------segundo grupo--------------- */}
-
-				<div id='groupInput' className=''>
-					<div>
-						<div id='divPrueba'>
-							<label id='labelPrueba'>Costo unitario</label>
-							<input
-								id='inputPrueba'
-								type='number'
-								name='costo'
-								value={form.costo}
-								onBlur={handleBlur}
-								onChange={handleChange}
-							></input>
-						</div>
-						{errors.costo && <p id='msgerror'>{errors.total}</p>}
-					</div>
-					<div>
-						<div id='divPrueba'>
-							<label id='labelPrueba'>Costo total</label>
-							<input
-								id='inputPrueba'
-								type='number'
-								name='total'
-								value={form.total}
-								onBlur={handleBlur}
-								onChange={handleChange}
-							></input>
-						</div>
-						{errors.total && <p id='msgerror'>{errors.total}</p>}
-					</div>
-				</div>
-
-				{/* -------------------tercer grupo--------------- */}
-
-				<div id='groupInput' className=''>
-					<div>
-						<div id='divPrueba'>
-							<label id='labelPrueba'>Precio</label>
-							<input id='inputPrueba'></input>
-						</div>
-						{errors.precio && <p id='msgerror'>{errors.precio}</p>}
-					</div>
-					<div>
-						<div id='divPrueba'>
-							<label id='labelPrueba'>Categoria</label>
-
-							<select
-								id='inputPrueba'
-								name='categorias'
-								// type='text'
-
-								// value={form.categorias}
-								onBlur={handleBlur}
-								onChange={handleChange}
 							>
-								<option id='' value=''>
-									Seleciona Categoria
-								</option>
-								<option id='Vegetales' value=''>
-									Vegetales
-								</option>
-								<option id='Snacks' value='Snacks'>
-									Snacks
-								</option>
-								<option id='Lacteos' value='	Lacteos'>
-									Lacteos
-								</option>
-								<option id='Limpieza' value='Limpieza'>
-									Limpieza
-								</option>
-								<option id='Bebidas' value='Bebidas'>
-									Bebidas
-								</option>
-							</select>
-						</div>
-						{errors.categoria && <p id='msgerror'>{errors.categoria}</p>}
-					</div>
-				</div>
-			</div>
-		</div>
+								{/* --------------input nombre------------- */}
+								<div className=' w-341 h-88 flex flex-col m-1 pl-2 gap-2 items-center md:w-607 md:mt-0 md:mb-0 md:h-18 md:pb-2'>
+									<div>
+										<div className='flex flex-col ml-25 pl-2'>
+											<label className='w-48 h-6 text-start'>Nombre</label>
+											<input
+												type='text'
+												id='nombre'
+												name='nombre'
+												value={form.nombre}
+												onBlur={handleBlur}
+												onChange={handleChange}
+												className='w-375 h-h48 bg-white border-solid border-1 border-secundario3 rounded-xl px-3 py-4 box-border md:w-556'
+											></input>
+										</div>
+										{errors.nombre && (
+											<p id='msgerror' className='ml-34'>
+												{errors.nombre}
+											</p>
+										)}
+									</div>
+								</div>
+								<div className='flex flex-col justify-center items-center w-screen'>
+									{/* -------------------primer grupo--------------- */}
 
-							 
-							
+									<div id='groupInput'>
+										<div>
+											<div id='divPrueba' className=''>
+												<label id='labelPrueba'>Cantidad</label>
+												<input
+													id='inputPrueba'
+													type='number'
+													name='cantidad'
+													value={form.cantidad}
+													onBlur={handleBlur}
+													onChange={handleChange}
+												></input>
+											</div>
+											{errors.cantidad && (
+												<p id='msgerror'>{errors.cantidad}</p>
+											)}
+										</div>
+										<div>
+											<div id='divPrueba'>
+												<label id='labelPrueba'>Seleciona la unidad</label>
+												<select
+													id='inputPrueba'
+													name='unidades'
+													onChange={handleChange}
+												>
+													<option id='' value=''>
+														Seleciona unidad
+													</option>
+													<option id='unidades' value='unidades'>
+														Unidades
+													</option>
+													<option id='Kg' value='Kg'>
+														Kg
+													</option>
+													<option id='Mts' value='Mts'>
+														Mts
+													</option>
+													<option id='Lts' value='Lts'>
+														Lts
+													</option>
+												</select>
+											</div>
+											{errors.unidades && (
+												<p id='msgerror'>{errors.unidades}</p>
+											)}
+										</div>
+									</div>
+
+									{/* -------------------segundo grupo--------------- */}
+
+									<div id='groupInput' className=''>
+										<div>
+											<div id='divPrueba'>
+												<label id='labelPrueba'>Costo unitario</label>
+												<input
+													id='inputPrueba'
+													type='number'
+													name='costo'
+													value={form.costo}
+													onBlur={handleBlur}
+													onChange={handleChange}
+												></input>
+											</div>
+											{errors.costo && <p id='msgerror'>{errors.total}</p>}
+										</div>
+										<div>
+											<div id='divPrueba'>
+												<label id='labelPrueba'>Costo total</label>
+												<input
+													id='inputPrueba'
+													type='number'
+													name='total'
+													value={form.total}
+													onBlur={handleBlur}
+													onChange={handleChange}
+												></input>
+											</div>
+											{errors.total && <p id='msgerror'>{errors.total}</p>}
+										</div>
+									</div>
+
+									{/* -------------------tercer grupo--------------- */}
+
+									<div id='groupInput' className=''>
+										<div>
+											<div id='divPrueba'>
+												<label id='labelPrueba'>Precio</label>
+												<input id='inputPrueba'></input>
+											</div>
+											{errors.precio && <p id='msgerror'>{errors.precio}</p>}
+										</div>
+										<div>
+											<div id='divPrueba'>
+												<label id='labelPrueba'>Categoria</label>
+
+												<select
+													id='inputPrueba'
+													name='categorias'
+													// type='text'
+
+													// value={form.categorias}
+													onBlur={handleBlur}
+													onChange={handleChange}
+												>
+													<option id='' value=''>
+														Seleciona Categoria
+													</option>
+													<option id='Vegetales' value=''>
+														Vegetales
+													</option>
+													<option id='Snacks' value='Snacks'>
+														Snacks
+													</option>
+													<option id='Lacteos' value='	Lacteos'>
+														Lacteos
+													</option>
+													<option id='Limpieza' value='Limpieza'>
+														Limpieza
+													</option>
+													<option id='Bebidas' value='Bebidas'>
+														Bebidas
+													</option>
+												</select>
+											</div>
+											{errors.categoria && (
+												<p id='msgerror'>{errors.categoria}</p>
+											)}
+										</div>
+									</div>
+								</div>
+								{/* -------------cuarto grupo----------------- */}
+    <div className=''>
+        <button
+            className='w-40 h-h48  top-96   md:top-418 md:w-266 left-4 rounded-xl p-2.5 gap-2.5 bg-acento2 flex flex-row justify-center items-center absolute'
+            onClick={() =>setModal(true)}
+        >
+            <div className=' text-primario w-120 h-22 font-secundaria not-italic font-bold text-base  grow-0order-0 '>
+                Cargar Excel
+            </div>
+        </button>
+
+        <button
+            type='submit'
+            value='send'
+            onClick={() => setVisible(true)}
+            className='w-40 h-h48 top-96 md:top-418 md:w-266 left-200 md:left-305 rounded-xl p-2.5 gap-2.5 bg-secundario flex flex-row justify-center items-center absolute'
+        >
+            <div className=' text-primario font-secundaria w-78 h-22 font-bold text-base not-italic  order-0 grow-0 '>
+                Continuar
+            </div>
+        </button>
+
+        {visible ? <ModalProductocargado texto={"Productos cargados exitosamente!"}/> : null}
+        {modal ? <ModalExcel setModal={setModal} /> : null}
+        {/* {response? null:  <ModalFallaCarga setVisible={setVisible}/>} */}
+    </div>
+							</div>
+
 						</form>
 					</div>
 				</div>
