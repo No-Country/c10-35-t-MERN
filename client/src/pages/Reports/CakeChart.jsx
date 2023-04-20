e23a9f9dab941540e605ec7cb91e68c31375c315
javier marquez <hello.jmarquez@gmail.com>
2023-04-19 23:30:17 -0500

    fix: small fix

--- client/src/pages/Reports/CakeChart.jsx
+++ client/src/pages/Reports/CakeChart.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react'
-
 import { Line } from 'react-chartjs-2'
-
 import { Chart as Chartjs } from 'chart.js/auto'
 
 function CakeChart({ charData }) {
